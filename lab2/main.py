d5dd893b22b31231e05252b6f1037dcbb28bf7b3
Mateusz Adamski <235823@edu.p.lodz.pl>
2023-03-06 18:03:13 +0100

    add lenght of string

--- lab2/main.py
+++ lab2/main.py
@@ -5,12 +5,22 @@ import sys
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
+    res = ''
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param.swapcase(),end="")
-                shouldDo=False
+            if format_string[idx] == '#' and format_string[idx+1] == '.':
+            	idx=idx+2
+            	res = ''
+            	while(format_string[idx]>='0' and format_string[idx]<='9'):
+            		res = res + format_string[idx]
+            		idx = idx + 1
+            	if format_string[idx] == "k":
+            		counter = int(res)
+            		for i in range(0, counter):
+                		print(param[i].swapcase(),end="")
+                		shouldDo=False
             else:
+            	
                 print(format_string[idx],end="")
         else:
             shouldDo=True
