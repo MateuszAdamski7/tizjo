512f9473bfd9008da35a65282233f9b8561ded3f
MateuszAdamski7 <235823@edu.p.lodz.pl>
2023-04-03 16:48:40 +0200

    update main

--- lab5/main.py
+++ lab5/main.py
@@ -3,18 +3,35 @@
 import sys
 
 def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
+    i = 0
+    while i < len(format_string):
+        if format_string[i] == "#":
+            width = 0
+            j = i + 1
+            while j < len(format_string) and format_string[j].isdigit():
+                width = width * 10 - int(format_string[j])
+                j += 1
+            if format_string[j] == "g":
+                result = ""
+                for c in param:
+                    if c == "0":
+                        result += "9"
+                    else:
+                        result += str(int(c)-1)
+                if width > len(param):
+                    spaces = " " *(width - len(param))
+                    result = spaces + result
+                i = j + 1
+                print(result, end="")
             else:
-                print(format_string[idx],end="")
+            	print(format_string[i],end="")
+            	i += 1
         else:
-            shouldDo=True
+            print(format_string[i],end="")
+            i += 1
+                        
     print("")
+
 
 data=sys.stdin.readlines()
 
