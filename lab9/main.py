3fc88cff426c515f9a8494c7b0995f8dec553553
Mateusz Adamski <235823@edu.p.lodz.pl>
2023-05-22 18:17:20 +0200

    update main

--- lab9/main.py
+++ lab9/main.py
@@ -2,18 +2,56 @@
 
 import sys
 
-def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
+def my_printf(format_string, param):
+    i = 0
+    width = 0
+    result = ""
+    while i < len(format_string):
+        if format_string[i:i+2] == '#.':
+            j = i + 2
+            while j < len(format_string) and format_string[j].isdigit():
+                j += 1
+            if j < len(format_string) and format_string[j] == 'h':
+                width = 0
+                if format_string[i + 2:j].isdigit():
+                    width = int(format_string[i + 2:j])
+                else:
+                    i -= 1
+            res = divmod(param,1)
+            new_res = ""
+            hel = str(int(res[0]))
+            for c in hel:
+                if c.isdigit():
+                    kod = ord('a') + int(c)
+                    lit = chr(kod)
+                    new_res += lit
+
+            new_res += '.'
+
+            len_dec = len(str(param)) - len(hel) - 1
+            e = str(param)
+            aa = e[-len_dec:]
+            print(aa)
+            iter = 0
+            for c in aa:
+                if iter >= width:
+                    break
+                iter += 1
+                if c.isdigit():
+                    nc = (int(c) + 5) % 10
+                    new_res += str(nc)
+            hel_len =  width - len(aa)
+            if hel_len > 0:
+                for c in range(hel_len):
+                    new_res += "0"
+            result += new_res
+
+            i += 3 + len(str(width))
+            continue
         else:
-            shouldDo=True
+            result += format_string[i]
+            i += 1
+    print(result, end="")
     print("")
 
 data=sys.stdin.readlines()
