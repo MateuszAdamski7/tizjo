d71bed814a8de5b865dbf8f3a5a7d4d4d52e50ec
Mateusz Adamski <235823@edu.p.lodz.pl>
2023-06-05 17:44:45 +0200

    update main

--- lab11/main.py
+++ lab11/main.py
@@ -3,17 +3,35 @@
 import sys
 
 def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
+    i = 0
+    result = ""
+    counter = 0
+    while i < len(format_string):
+        if format_string[i] == '#':
+            if format_string[i+1] != 'b':
+                i += 1
+                result += '#'
+                continue
+
+            param = int(param)
+            binary = format(param, 'b')
+            reversed_binary = str(binary)
+            reversed_binary = reversed_binary[::-1]
+            res = ""
+            for c in reversed_binary:
+                if c == '1':
+                    if counter == 10:
+                        counter = 0
+                    res += chr(ord(c) + 48 + counter)
+                else:
+                    res += c
+                counter += 1
+            result += res[::-1]
+            i += 2
         else:
-            shouldDo=True
+            result += format_string[i]
+            i += 1
+    print(result, end="")
     print("")
 
 data=sys.stdin.readlines()
